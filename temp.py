10f90b675761d2fd1278e07f8309bafe5b906cdd
Miguel de Jesús Martínez Felipe <mmartinezf2002@alumno.ipn.mx>
2024-04-11 16:28:03 -0600

    Intersection Face and Quad
    11/04/2024

--- temp.py
+++ temp.py
@@ -25,6 +25,11 @@ print("Version of software 2.0.1")
 "ADAPTIVE_THRESH_MEAN_C"
 "dilate"
 
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
 def angle_cos(p0, p1, p2):
     d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
     #print(type(d1))
@@ -32,15 +37,30 @@ def angle_cos(p0, p1, p2):
     #print(np.dot(d1, d2))
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
 
+def backgound_cnt(img):
+    _, thresholded = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+
+    # Find contours
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the contour with the largest area
+    largest_contour = max(contours, key=cv2.contourArea)
+
+    # Calculate the area of the largest contour
+    background_area = cv2.contourArea(largest_contour)
+    return background_area
 
 def find_quads(img):
     #ret, bin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+ cv2.THRESH_OTSU)
     #print(ret)
+    # Threshold the image to segment the background
+    #print(backgound_cnt(img))
+    height, width = img.shape
     squares = []
     blurred = cv2.GaussianBlur(img,(11,11),0)
         #bin = cv2.Canny(blurred, cv2.THRESH_OTSU, 255)# apertureSize = 5; cv2.THRESH_OTSU, revuelve 2 objetos ret, 
 
-    cv2.imshow("blur", blurred)
+    #cv2.imshow("blur", blurred)
     bin = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
         # bin = cv2.dilate(bin, kernel, iterations=1)
@@ -49,26 +69,35 @@ def find_quads(img):
         #cv2.imshow("Dilate", bin)
         #cv2.imshow("open", bin)
     
-    cv2.imshow("result", bin)
+    #cv2.imshow("result", bin)
     
     contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         cnt_len = cv2.arcLength(cnt, True)
         cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True) #0.02
-        print("cnt Before condition",cnt)
-        if len(cnt) == 4 and cv2.contourArea(cnt) > 7000: #and cv2.isContourConvex(cnt):
-            print("if have 4 points and it is greater than 7000 pixels",cnt)
-            print("contourArea",cv2.contourArea(cnt))        
+        #print("cnt Before condition",cnt)
+        if len(cnt) == 4 and cv2.contourArea(cnt) > 7000 and cv2.contourArea(cnt)<((height*width)*.995): #and cv2.isContourConvex(cnt):
+            #print("if have 4 points and it is greater than 7000 pixels",cnt)
+            #print("contourArea",cv2.contourArea(cnt))        
             cnt = cnt.reshape(-1, 2)
             max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
-            print("max_cos general",max_cos)
+            #print("max_cos general",max_cos)
             if max_cos < 0.3:  # 0.3
-                print("max_cos appoved",cnt)
+                #print("max_cos appoved",cnt)
                 squares.append(cnt)
             
     return squares
 
 
+
+def detect_bounding_box(img):
+    faces = face_classifier.detectMultiScale(img, 1.1, 5, minSize=(40, 40))
+    cropped_faces = []
+    
+    for (x, y, w, h) in faces:
+        cropped_faces.append(img[y:y+h, x:x+w])
+        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return cropped_faces
 
 
 cap_video = cv2.VideoCapture(0)
@@ -92,15 +121,42 @@ while(True):
     ret, frame = cap_video.read()
     if ret == True:
         # Use de image to convert in gray scale
+        
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #result.write(frame)
-        # Use de image to obtain canny edge
+        
+        faces = detect_bounding_box(img)
+        ##Agregar código para saber si face se encuentra dentro del cuadrilatero
+        ##Después de revisar el rostro hacer un análisis de los píxeles al rededor
+        #por análissi de textura, local binary pattern, frecuency domain
         quads = find_quads(img)
+        #print("quads",quads)
+        #result = []
+        threshold_similarity = 0.8  
+        
+        
+        ##Normalizar la profundad de Face y Quad
+        if(len(faces) != 0) and (len(quads) != 0):
+            for face in faces:
+                for quad in quads:
+                    if len(face.shape) > 2:
+                        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+                    if len(quad.shape) > 2:
+                        quad = cv2.cvtColor(quad, cv2.COLOR_BGR2GRAY)
+                        
+                    result = cv2.matchTemplate(quad, face, cv2.TM_CCOEFF_NORMED)
+                    if result is not None:
+                        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                        if max_val > threshold_similarity:
+                            print("The smaller image is inside the larger image.")
+        
+        
+        
+        
+        #result = cv2.matchTemplate(larger_gray, smaller_gray, cv2.TM_CCOEFF_NORMED)
+        #quads = find_quads(img)
         cv2.drawContours(img, quads, -1, (255, 0, 0), 3 )
         cv2.imshow('quads', img)
-        #cv2.imwrite("quad"+ str(time.time())+'.png', img)
-        #cv2.imshow("OpenCVCam", frame)
-    
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
